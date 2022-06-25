fe6da497d1ecc36b50d387727f0aa48e84ba7491
Yann Heeser <yann@heeser.io>
2022-06-25 15:19:28 +0200

    Go mod support Update redis to v8 for ttl

--- adapter/redis/redis.go
+++ adapter/redis/redis.go
@@ -25,17 +25,18 @@ SOFTWARE.
 package redis
 
 import (
+	"context"
 	"time"
 
-	redisCache "github.com/go-redis/cache"
-	"github.com/go-redis/redis"
+	redisCache "github.com/go-redis/cache/v8"
+	"github.com/go-redis/redis/v8"
 	cache "github.com/victorspringer/http-cache"
-	"github.com/vmihailenco/msgpack"
+	msgpack "github.com/vmihailenco/msgpack/v5"
 )
 
 // Adapter is the memory adapter data structure.
 type Adapter struct {
-	store *redisCache.Codec
+	store *redisCache.Cache
 }
 
 // RingOptions exports go-redis RingOptions type.
@@ -44,7 +45,7 @@ type RingOptions redis.RingOptions
 // Get implements the cache Adapter interface Get method.
 func (a *Adapter) Get(key uint64) ([]byte, bool) {
 	var c []byte
-	if err := a.store.Get(cache.KeyAsString(key), &c); err == nil {
+	if err := a.store.Get(context.Background(), cache.KeyAsString(key), &c); err == nil {
 		return c, true
 	}
 
@@ -54,23 +55,24 @@ func (a *Adapter) Get(key uint64) ([]byte, bool) {
 // Set implements the cache Adapter interface Set method.
 func (a *Adapter) Set(key uint64, response []byte, expiration time.Time) {
 	a.store.Set(&redisCache.Item{
-		Key:        cache.KeyAsString(key),
-		Object:     response,
-		Expiration: expiration.Sub(time.Now()),
+		Key:   cache.KeyAsString(key),
+		Value: response,
+		TTL:   time.Until(expiration),
 	})
 }
 
 // Release implements the cache Adapter interface Release method.
 func (a *Adapter) Release(key uint64) {
-	a.store.Delete(cache.KeyAsString(key))
+	a.store.Delete(context.Background(), cache.KeyAsString(key))
 }
 
 // NewAdapter initializes Redis adapter.
 func NewAdapter(opt *RingOptions) cache.Adapter {
 	ropt := redis.RingOptions(*opt)
+	rc := redis.NewRing(&ropt)
 	return &Adapter{
-		&redisCache.Codec{
-			Redis: redis.NewRing(&ropt),
+		redisCache.New(&redisCache.Options{
+			Redis: rc,
 			Marshal: func(v interface{}) ([]byte, error) {
 				return msgpack.Marshal(v)
 
@@ -78,6 +80,6 @@ func NewAdapter(opt *RingOptions) cache.Adapter {
 			Unmarshal: func(b []byte, v interface{}) error {
 				return msgpack.Unmarshal(b, v)
 			},
-		},
+		}),
 	}
 }
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/heeser-io/http-cache
+
+go 1.18
+
+require (
+	github.com/allegro/bigcache v1.2.1
+	github.com/go-redis/cache/v8 v8.4.3
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/victorspringer/http-cache v0.0.0-20220131145941-ef3624e6666f
+	github.com/vmihailenco/msgpack/v5 v5.3.5
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/stretchr/testify v1.7.5 // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/exp v0.0.0-20210916165020-5cb4fee858ee // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
